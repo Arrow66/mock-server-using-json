3e1688e961cf8228f126ea6fb51e22045b441079
Arjun Biju <arjunan8080@gmail.com>
2020-05-21 22:02:50 +0530

    cors changes

--- index.js
+++ index.js
@@ -1,77 +1,56 @@
-const express = require("express")
+const express = require("express");
 var fs = require("fs");
 var url = require("url");
+const chalk = require('chalk');
+const cors = require("cors");
 const app = express();
 const port = 3001;
 
+try {
+  const jsonFile = JSON.parse(fs.readFileSync("./utils/main.json"));
 
-try
-{
-const jsonFile = JSON.parse(fs.readFileSync("./utils/main.json"));
+  app.use(cors());
+  app.use(express.json());
 
+  app.use((req,res,next)=>{
 
+    console.log(chalk.green(req.url));
+    next();
 
-app.use((req,res,next)=>{
+  })
 
-    try
-    {
- 
-    let result =  jsonFile.find((el)=>{
-
-       let path =  url.parse(el.url).pathname
-
-       console.log("pathname",path)
-        return  (path == req.url)&&(el.method == req.method) 
-        
-     })
-
-     if(result)
-     {
-         res.json(
-            JSON.parse(result.response)
-        )
-
-     }else
-     {
-
-        res.status(404).send("No routes")
-
-     }
-
-    }
-    catch (err)
-    {
-
-        console.log(err)
-        res.status(404).send("No routes")
-
-    }
-
+  app.all("*",(req, res, next) => {
+      try
+      {
     
+      let result = jsonFile.find((el,i) => {
+        let path = url.parse(el.url).path;
 
+        return path == req.url && el.method == req.method;
+      });
 
+      if (result) {
+        res.json(JSON.parse(result.response));
+      } else {
+        res.status(404).json({
+          error: "no route"
+        });
+      }
 
+      }
+      catch(err)
+      {
+          res.status(404).json(
+              {
+                  error:"no route"
+              }
+          )
+      }
+  });
 
-
-})
-
-
-
-
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-
+  app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+  );
+} catch (err) {
+  console.log("Error");
 }
-catch(err)
-{
-    console.log("Error")
-}
-
-
-
-
-
-
-
-
